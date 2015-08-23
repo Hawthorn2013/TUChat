6d095bef51a0ce6e5ccced6bb945f188195d4fbb
Jian JIAO <mynetuser@outlook.com>
2015-08-24 01:46:35 +0800

    实现自动滚动

--- app/src/main/java/com/tusmartcar/jian/tuchat/MainActivity.java
+++ app/src/main/java/com/tusmartcar/jian/tuchat/MainActivity.java
@@ -10,7 +10,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -28,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private ServiceConnection conn = null;
     private TextView ConnectionStatus = null;
     private BufferedReader bufferedReader = null;
+    private CheckBox cbAutoScroll = null;
+    private Button btnClearHistory = null;
+    private ScrollView svMainShowText = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mainShowText = (TextView)findViewById(R.id.MainShowText);
         mainEditText = (EditText)findViewById(R.id.MainEditText);
+        cbAutoScroll = (CheckBox)findViewById(R.id.cbAutoScroll);
+        cbAutoScroll.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            svMainShowText.fullScroll(ScrollView.FOCUS_DOWN);
+                        }
+                    });
+                }
+            }
+        });
+        btnClearHistory = (Button)findViewById(R.id.btnClearHistory);
         mainTextSend = (Button)findViewById(R.id.MainTextSend);
         mainTextSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 sendDataBinder.SendMsg(new String("I am Nexus 5."));
             }
         });
+        svMainShowText = (ScrollView)findViewById(R.id.svMainShowText);
 
         ConnectionStatus = (TextView)findViewById(R.id.ConnectionStatus);
         mHandler = new Handler() {
@@ -52,6 +75,14 @@ public class MainActivity extends AppCompatActivity {
                 switch (msg.what) {
                     case SocketService.SocketService_to_MainActiviyt :
                         mainShowText.setText(mainShowText.getText() + System.getProperty("line.separator") + (String)(msg.obj));
+                        if (cbAutoScroll.isChecked()) {
+                            mHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    svMainShowText.fullScroll(ScrollView.FOCUS_DOWN);
+                                }
+                            });
+                        }
                         break;
                     case SocketService.ConnectionStatus_Connected :
                         ConnectionStatus.setText("已连接^-^");
